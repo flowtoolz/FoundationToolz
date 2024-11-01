5e0e0d6625aa521fc7c876b8f2c3cecfbf8cc583
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-11-01 21:52:15 +0100

    Add new implementation of HTTP requests for flexible usage of web APIs

---
+++ Code/Foundation+FoundationToolz/HTTP.swift
@@ -0,0 +1,212 @@
+import Foundation
+
+@available(macOS 14.0, *)
+public enum HTTP {
+    static func sendRequest<Response: Decodable>(
+        to endpoint: URLString,
+        using method: Method = .GET,
+        content: Encodable? = nil,
+        authorizationValue: String? = nil,
+        addingHeaders headersToAdd: [String: String]? = nil,
+        timeoutInterval: Duration = .seconds(10)
+    ) async throws(RequestError) -> Response {
+        let url: URL
+        
+        do {
+            url = try URL(validating: endpoint)
+        } catch {
+            throw .invalidURLString(error)
+        }
+        
+        return try await sendRequest(to: url,
+                                     using: method,
+                                     content: content,
+                                     authorizationValue: authorizationValue,
+                                     addingHeaders: headersToAdd,
+                                     timeoutInterval: timeoutInterval)
+    }
+    
+    static func sendRequest<Response: Decodable>(
+        to endpoint: URL,
+        using method: Method = .GET,
+        content: Encodable? = nil,
+        authorizationValue: String? = nil,
+        addingHeaders headersToAdd: [String: String]? = nil,
+        timeoutInterval: Duration = .seconds(10)
+    ) async throws(RequestError) -> Response {
+        let (responseContent, httpResponse) = try await sendRequest(to: endpoint,
+                                                                       using: method,
+                                                                       content: content,
+                                                                       authorizationValue: authorizationValue,
+                                                                       addingHeaders: headersToAdd,
+                                                                       timeoutInterval: timeoutInterval)
+        
+        guard (200 ... 299).contains(httpResponse.statusCode) else {
+            throw .unexpectedStatusCode(httpResponse, responseContent)
+        }
+        
+        do {
+            return try JSONDecoder().decode(Response.self, from: responseContent)
+        } catch {
+            throw RequestError(error)
+        }
+    }
+    
+    static func sendRequest(
+        to endpoint: URL,
+        using method: Method = .GET,
+        content: Encodable? = nil,
+        authorizationValue: String? = nil,
+        addingHeaders headersToAdd: [String: String]? = nil,
+        timeoutInterval: Duration = .seconds(10)
+    ) async throws(RequestError) -> (Data, HTTPURLResponse) {
+        var urlRequest = URLRequest(url: endpoint)
+        
+        urlRequest.httpMethod = method.rawValue
+        
+        if let authorizationValue {
+            urlRequest.setValue(authorizationValue, forHTTPHeaderField: "Authorization")
+        }
+        
+        for (field, value) in (headersToAdd ?? [:]) {
+            urlRequest.addValue(value, forHTTPHeaderField: field)
+        }
+        
+        let (responseContent, response): (Data, URLResponse)
+        
+        do {
+            if let content {
+                urlRequest.httpBody = try JSONEncoder().encode(content)
+                urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+            
+            (responseContent, response) = try await withTimeout(after: timeoutInterval) {
+                try await URLSession.shared.data(for: urlRequest)
+            }
+        } catch {
+            throw RequestError(error)
+        }
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw .noHTTPResponse(response, responseContent)
+        }
+        
+        return (responseContent, httpResponse)
+    }
+    
+    enum Method: String {
+        case GET, POST, PUT, DELETE, PATCH
+    }
+    
+    public enum RequestError: Error, CustomStringConvertible {
+        init(_ error: Error) {
+            switch error {
+            case let invalidStringError as InvalidURLStringError:
+                self = .invalidURLString(invalidStringError)
+            case let encodingError as EncodingError:
+                self = .encodingError(encodingError)
+            case let timeoutError as TimeoutError:
+                self = .timeout(timeoutError)
+            case let urlError as URLError:
+                self = .urlError(urlError)
+            case let decodingError as DecodingError:
+                self = .decodingError(decodingError)
+            default:
+                self = .unexpectedError(error)
+            }
+        }
+        
+        public var description: String {
+            switch self {
+            case .invalidURLString(let invalidURLStringError):
+                "💥 Invalid URL string: \"" + invalidURLStringError.invalidURLString.value + "\""
+            case .encodingError(let encodingError):
+                "💥 Encoding error (reason: \(encodingError.failureReason ?? "nil")): " + encodingError.localizedDescription
+            case .timeout(let timeoutError):
+                "💥 Timeout after " + timeoutError.duration.description
+            case .urlError(let urlError):
+                "💥 URL error (error code: \(urlError.code.rawValue)): " + urlError.localizedDescription
+            case .noHTTPResponse(_, let responseContent):
+                "💥 Response is not an HTTP response. Response content: " + responseContent.asString
+            case .unexpectedStatusCode(let httpResponse, let responseContent):
+                "💥 Unexpected HTTP response status code \(httpResponse.statusCode). Response content: " + responseContent.asString
+            case .decodingError(let decodingError):
+                "💥 Decoding error (reason: \(decodingError.failureReason ?? "nil")): " + decodingError.localizedDescription
+            case .unexpectedError(let error):
+                "💥 Unexpected error: " + error.localizedDescription
+            }
+        }
+        
+        case invalidURLString(InvalidURLStringError)
+        case encodingError(EncodingError)
+        case timeout(TimeoutError)
+        case urlError(URLError)
+        case noHTTPResponse(URLResponse, Data)
+        case unexpectedStatusCode(HTTPURLResponse, Data)
+        case decodingError(DecodingError)
+        case unexpectedError(Error)
+    }
+}
+
+extension Data {
+    var asString: String {
+        String(data: self, encoding: .utf8) ?? debugDescription
+    }
+}
+
+@available(macOS 14.0, *)
+extension URL {
+    init(validating urlString: URLString) throws(InvalidURLStringError) {
+        if let url = URL(string: urlString.value, encodingInvalidCharacters: false) {
+            self = url
+        } else {
+            throw InvalidURLStringError(invalidURLString: urlString)
+        }
+    }
+}
+
+public struct InvalidURLStringError: Error {
+    let invalidURLString: URLString
+}
+
+public struct URLString: ExpressibleByStringLiteral {
+    public init(stringLiteral value: String) {
+        self.value = value
+    }
+    
+    init(_ value: String) {
+        self.value = value
+    }
+    
+    let value: String
+}
+
+@available(macOS 13.0, *)
+func withTimeout<Result>(
+    after duration: Duration,
+    startLongOperation: @escaping () async throws -> Result
+) async throws -> Result {
+    try await withThrowingTaskGroup(of: Result.self) { tasks in
+        tasks.addTask {
+            try await startLongOperation()
+        }
+        
+        tasks.addTask {
+            try await Task.sleep(for: duration)
+            throw TimeoutError(duration: duration)
+        }
+        
+        guard let result = try await tasks.next() else {
+            /// will never happen since `tasks` is not empty
+            throw CancellationError()
+        }
+        
+        tasks.cancelAll()
+        return result
+    }
+}
+
+@available(macOS 13.0, *)
+public struct TimeoutError: Error {
+    let duration: Duration
+}
--- Code/Foundation+FoundationToolz/URL+HTTPRequests.swift
+++ Code/Foundation+FoundationToolz/URL+HTTPRequests.swift
@@ -1,7 +1,8 @@
 import Foundation
 import SwiftyToolz
 
-@available(macOS 12.0, iOS 15.0, tvOS 13.0.0, watchOS 6.0.0, *)
+// TODO: check where this extension is used and dissolve it, it's unnecessary. use directly HTTP instead.
+@available(macOS 14.0, iOS 15.0, tvOS 13.0.0, watchOS 6.0.0, *)
 public extension URL
 {
     /**
@@ -9,138 +10,19 @@ public extension URL
      
      This only works when the server returns a valid JSON encoding of `Value`! It might not work for example when the server returns a pure unencoded String. In that case, use `func get() async throws -> Data` instead!
     **/
-    func get<Value: Decodable>(_ type: Value.Type = Value.self) async throws -> Value
+    func get<Value: Decodable>(_ type: Value.Type = Value.self) async throws(HTTP.RequestError) -> Value
     {
-        let (data, httpResponse) = try await getDataAndResponse()
-        
-        guard let value = Value(data) else
-        {
-            throw RequestError.decodingDataFailed(httpResponse, data)
-        }
-        
-        return value
+        try await HTTP.sendRequest(to: self)
     }
     
     /// Get request returning the pure data that the server sends in response
-    func get() async throws -> Data
+    func get() async throws(HTTP.RequestError) -> Data
     {
-        try await getDataAndResponse().0
-    }
-    
-    private func getDataAndResponse() async throws -> (Data, HTTPURLResponse)
-    {
-        do
-        {
-            let (data, response) = try await URLSession.shared.data(from: self)
-            
-            let httpResponse = response as! HTTPURLResponse
-            
-            guard (200 ... 299).contains(httpResponse.statusCode) else
-            {
-                throw RequestError.validatingResponseStatusFailed(httpResponse, data)
-            }
-            
-            return (data, httpResponse)
-        }
-        catch let requestError as RequestError
-        {
-            log(error: requestError.description)
-            throw requestError
-        }
-        catch let nsError as NSError
-        {
-            let isURLError = nsError.domain == URLError.errorDomain
-            let urlErrorCode = isURLError ? URLError.Code(rawValue: nsError.code) : nil
-            let requestError = RequestError.requestFailed(nsError, urlErrorCode)
-            log(error: requestError.description)
-            throw requestError
-        }
-        catch
-        {
-            log(warning: "URL request produced error of unexpected type \(typeName(error))")
-            let readableError = ReadableError(error.localizedDescription)
-            log(readableError)
-            throw readableError
-        }
+        try await HTTP.sendRequest(to: self).0
     }
     
     func post<Value: Encodable>(_ value: Value) async throws
     {
-        guard let valueData = value.encode() else
-        {
-            let requestError = RequestError.encodingDataFailed
-            log(error: requestError.description)
-            throw requestError
-        }
-        
-        var request = URLRequest(url: self)
-        request.httpMethod = "POST"
-        request.httpBody = valueData
-        
-        do
-        {
-            let (data, response) = try await URLSession.shared.data(for: request)
-            
-            let httpResponse = response as! HTTPURLResponse
-            
-            guard (200...299).contains(httpResponse.statusCode) else
-            {
-                throw RequestError.validatingResponseStatusFailed(httpResponse, data)
-            }
-        }
-        catch let nsError as NSError
-        {
-            let isURLError = nsError.domain == URLError.errorDomain
-            let urlErrorCode = isURLError ? URLError.Code(rawValue: nsError.code) : nil
-            let requestError = RequestError.requestFailed(nsError, urlErrorCode)
-            log(error: requestError.description)
-            throw requestError
-        }
-        catch let requestError as RequestError
-        {
-            log(error: requestError.description)
-            throw requestError
-        }
-    }
-    
-    enum RequestError: Error, CustomStringConvertible, CustomDebugStringConvertible, ReadableErrorConvertible
-    {
-        public var readableErrorMessage: String { description }
-        
-        public var localizedDescription: String { description }
-        
-        public var debugDescription: String { description }
-        
-        public var description: String
-        {
-            switch self
-            {
-            case .encodingDataFailed:
-                return "Could not endecode the data."
-            case .requestFailed(let nsError, let urlErrorCode):
-                var message = nsError.localizedDescription
-                if let urlErrorCode = urlErrorCode
-                {
-                    message += " URL error code: \(urlErrorCode.rawValue)"
-                }
-                return message
-            case .decodingDataFailed(let response, _):
-                let status = HTTPURLResponse.localizedString(forStatusCode: response.statusCode)
-                return "Could not decode the data. HTTP Status: " + status
-            case .validatingResponseStatusFailed(let response, let data):
-                let status = HTTPURLResponse.localizedString(forStatusCode: response.statusCode)
-                var message = "Unexpected HTTP Status: " + status
-                if let dataString = data.utf8String
-                {
-                    message += "\nResponse data: " + dataString
-                }
-                return message
-            }
-        }
-        
-        case encodingDataFailed
-        case requestFailed(NSError, URLError.Code?)
-        case validatingResponseStatusFailed(HTTPURLResponse, Data)
-        case decodingDataFailed(HTTPURLResponse, Data)
+        try await _ = HTTP.sendRequest(to: self, using: .POST, content: value)
     }
 }
